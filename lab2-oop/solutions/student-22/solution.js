9c5e8e5bb091c556296fb17ba4e6d63ad060c5ad
RiGGeRXVI <nikita116ru@gmail.com>
2025-09-19 23:15:30 +0300

    Lab2 student-22 not ready

--- lab2-oop/solutions/student-22/solution.js
+++ lab2-oop/solutions/student-22/solution.js
@@ -2,28 +2,37 @@
 
 // ===== ЗАДАНИЕ 1: Базовый класс Vehicle =====
 class Vehicle {
+    static vehicleCount = 0;
     // Создайте базовый класс Vehicle.
     // В конструкторе принимайте и сохраняйте в this свойства: 
     // make (марка), model (модель), year (год выпуска).
     constructor(make, model, year) {
-        // ..
+        Vehicle.vehicleCount++;
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
 
     // Добавьте метод displayInfo(), который выводит в консоль информацию 
     // о транспортном средстве в формате: "Марка: [make], Модель: [model], Год: [year]".
     displayInfo() {
-        // ..
+        console.log(`Марка: ${this.make}, Модель: ${this.model}, Год: ${this.year}`);
     }
 
     // Добавьте геттер age, который возвращает возраст транспортного средства 
     // (текущий год минус год выпуска). Используйте new Date().getFullYear().
     get age() {
-        // ..
+        return new Date().getFullYear() - this._year;
     }
 
     // Добавьте сеттер для года выпуска с проверкой: год не может быть больше текущего.
     set year(newYear) {
-        // ..
+        const yearNow = new Date().getFullYear();
+        if(newYear > yearNow) {
+            console.error("Новый год выпуска не должен быть больше нынешнего года выпуска");
+            return;
+        }
+        this._year = newYear;
     }
 
     get year() {
@@ -33,7 +42,11 @@ class Vehicle {
     // Добавьте статический метод compareAge(vehicle1, vehicle2), 
     // который возвращает разницу в возрасте между двумя транспортными средствами.
     static compareAge(vehicle1, vehicle2) {
-        // ..
+        return Math.abs(vehicle1.age - vehicle2.age);
+    }
+
+    static getTotalVehicles() {
+        return Vehicle.vehicleCount;
     }
 }
 
@@ -42,18 +55,20 @@ class Car extends Vehicle {
     // Создайте дочерний класс Car, который наследуется от Vehicle.
     // Добавьте новое свойство numDoors (количество дверей).
     constructor(make, model, year, numDoors) {
-        // ..
+        super(make, model, year);
+        this.numDoors = numDoors;
     }
 
     // Переопределите метод displayInfo() так, чтобы он также выводил количество дверей. 
     // Используйте super.displayInfo() для вызова метода родителя.
     displayInfo() {
-        // ..
+        super.displayInfo();
+        console.log(`Количесвто дверей: ${this.numDoors}`)
     }
 
     // Добавьте метод honk(), который выводит "Beep beep!".
     honk() {
-        // ..
+        console.log("Beep beep!")
     }
 }
 
@@ -62,18 +77,20 @@ class ElectricCar extends Car {
     // Создайте дочерний класс ElectricCar, который наследуется от Car.
     // Добавьте новое свойство batteryCapacity (емкость батареи в кВт·ч).
     constructor(make, model, year, numDoors, batteryCapacity) {
-        // ..
+        super(make, model, year, numDoors)
+        this.batteryCapacity = batteryCapacity;
     }
 
     // Переопределите метод displayInfo() для вывода дополнительной информации о батарее.
     displayInfo() {
-        // ..
+        super.displayInfo();
+        console.log(`Ёмкость батареии: ${this.batteryCapacity} кВт·ч`);
     }
 
     // Добавьте метод calculateRange(), который рассчитывает примерный запас хода 
     // (предположим, что 1 кВт·ч = 6 км).
     calculateRange() {
-        // ..
+        return this.batteryCapacity * 6;
     }
 }
 
@@ -82,8 +99,8 @@ class ElectricCar extends Car {
 
 // Создайте функцию createVehicleFactory, которая возвращает функцию 
 // для создания транспортных средств определенного типа (каррирование).
-const createVehicleFactory = (vehicleType) => (make, model, year) => {
-    return {}; // Замените {} на варажение
+const createVehicleFactory = (vehicleType) => (make, model, year, ...args) => {
+    return new vehicleType(make, model, year, ...args); // Замените {} на варажение
 };
 
 
@@ -120,6 +137,23 @@ function runTests() {
     const testVehicle = new Vehicle('Test', 'Model', 2010);
     console.assert(testVehicle.age === (new Date().getFullYear() - 2010), 'Тест возраста провален');
     
+    // Проверка сеттера года
+    const currentYear = new Date().getFullYear();
+    testVehicle.year = currentYear - 1;
+    console.assert(testVehicle.year === currentYear - 1, 'Тест сеттера года провален');
+
+    // Попытка установить будущий год
+    testVehicle.year = currentYear + 1;
+    console.assert(testVehicle.year === currentYear - 1, 'Тест валидации года провален');
+
+    // Проверка сравнения возраста
+    const vehicle1 = new Vehicle('1', '2', 2000)
+    const vehicle2 = new Vehicle('3', '4', 2010)
+    const ageDiff = Vehicle.compareAge(vehicle1, vehicle2);
+    console.assert(ageDiff === 10, 'Тест сравнения возраста провален')
+
+    // Проверка каррирования
+
     const createCarFactory = createVehicleFactory(Car);
     const myNewCar = createCarFactory('BMW', 'X5', 2022);
     console.log('Создан новый автомобиль:');
